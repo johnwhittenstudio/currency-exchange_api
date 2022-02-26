8384b6a7d08f286609d485a1c587edbe4bcea866
John Whitten <johnwhitten.studio@gmail.com>
2022-02-26 13:30:30 -0800

    update package.json and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/currency-exchange_api/"
   },
   devtool: "eval-source-map",
   devServer: {
